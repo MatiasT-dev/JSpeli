498d1de51f80465d8eb9899d9cb1fe582c09c461
MatiasT-dev <158021604+MatiasT-dev@users.noreply.github.com>
2026-02-04 10:30:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -216,6 +216,9 @@
         pelipiste = new osansa(20, 20, "black", Math.floor(Math.random() * 460) + 9, Math.floor(Math.random() * 255) + 9);
         peliesto = new osansa(20, 20, "black", Math.floor(Math.random() * 460) + 9, Math.floor(Math.random() * 255) + 9);       
         pelinloppu = new osansa(20, 20, "black", Math.floor(Math.random() * 460) + 9, Math.floor(Math.random() * 255) + 9);     
+        pelipiste.updaid();
+        peliesto.updaid();
+        pelipala.updaid();
         peliarena.stop();
         }else{   
 
@@ -264,14 +267,7 @@
         function resepiste() {
         piste = 0;
         nopeus = 0.5;
-        pelipala = new osansa(30, 30, "white", 10, 120);
-        pelipiste = new osansa(20, 20, "green", Math.floor(Math.random() * 460) + 20, Math.floor(Math.random() * 255) + 20);
-        peliesto = new osansa(20, 20, "red",Math.floor(Math.random() * 460) + 20, Math.floor(Math.random() * 255) + 20);
-        pelinloppu = new osansa(20, 20, "blue",Math.floor(Math.random() * 460) + 9, Math.floor(Math.random() * 255) + 9);
-        pelipiste.updaid();
-        peliesto.updaid();
-        peliohi.updaid();
-        pelipala.updaid();
+        aloitapeli();
         paivitapiste();
         }
         //pisteiden talentamiseen
